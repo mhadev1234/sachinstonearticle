e33c388dc6fc412a689f3db78448491e67bf8201
Sudesh Saini <sudeshsaini244@gmail.com>
2026-08-04 21:16:26 +0530

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -147,6 +148,10 @@ export default function RootLayout({
   }}
 />
         {children}
+        
+
+<GoogleAnalytics gaId="G-S27MEMKVV2" />
+
       </body>
     </html>
   );
