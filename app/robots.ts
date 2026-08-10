94027973ed0ed06920b53b3503c945395ef8622c
Sudesh Saini <sudeshsaini244@gmail.com>
2026-08-10 23:43:09 +0530

    Improve robots configuration

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,13 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/admin/"],
     },
 
     sitemap: "https://sachinstonearticle.vercel.app/sitemap.xml",
-
-    host: "https://sachinstonearticle.vercel.app",
   };
 }
